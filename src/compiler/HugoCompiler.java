e9f9fffa425bcd0befcd83850c511831d1f75e7f
Carlos Benavides <carlos.benavides@digitas.com>
2019-09-15 17:32:42 -0600

    Writing to error logs file

--- src/compiler/HugoCompiler.java
+++ src/compiler/HugoCompiler.java
@@ -3,15 +3,20 @@ package compiler;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 
 
 public class HugoCompiler {
 
     
 
-    private void GenerateErrorFile(String fileName) throws Exception {
+    private void GenerateErrorFile(String fileName, String fileContent) throws Exception {
         final File file = new File("./", fileName);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(fileContent);
+        writer.close();
         file.createNewFile();
     }
 
@@ -22,7 +27,7 @@ public class HugoCompiler {
             // TODO: If there are errors, return error file
             
             final String errorFileName = fileName.substring(0, fileName.lastIndexOf('.')) + "-HUGO-errores.txt";
-            GenerateErrorFile(errorFileName);
+            GenerateErrorFile(errorFileName, "File content ");
         } catch (Exception e) {
             System.out.println("Hubo un error con el archivo de entrada.");
         }
